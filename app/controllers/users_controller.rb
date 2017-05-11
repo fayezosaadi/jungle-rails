5c44762935bf1c400ea22b8b9a4f197816ecebf2
fayezosaadi <fayezosaadi@gmail.com>
2017-05-11 20:02:14 +0000

    Feature: User Authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,16 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = user.id
-      redirect_to '/', notice: 'user created!'
+      session[:user_id] = @user.id
+      redirect_to '/', notice: 'Account created successfully'
     else
+      flash[:error] = 'Email already exists!'
       render :new
     end
   end
